70194e656321edd9f35cdbfeea333e27cd1324e5
yell-yoshua <usuario1529yoshua@hotmail.com>
2020-08-17 05:32:35 -0500

    test4

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var ghost = require('ghost');
 ghost()
   .then( ghostServer => ghostServer.start() )
   .catch( error => {
+    console.log({error})
     console.error(`Ghost server error: ${error.message} ${error.stack}`);
     process.exit(1);
   });
